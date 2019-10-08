44e6c312c3286c4c58b1fe40426bdbdd9edd0e58
Jason Stoltzfus <jastoltz24@gmail.com>
2019-10-08 13:46:35 -0400

    Added permissions

--- lib/elastic/enterprise-search/client/permissions.rb
+++ lib/elastic/enterprise-search/client/permissions.rb
@@ -4,24 +4,24 @@ module Elastic
     class Client
       module Permissions
 
-        def list_all_permissions
-
+        def list_all_permissions(content_source_key, current: 1, size: 20)
+          get("ent/sources/#{content_source_key}/permissions", "page[current]" => current, "page[size]" => size )
         end
 
-        def get_user_permissions
-
+        def get_user_permissions(content_source_key, user)
+          get("ent/sources/#{content_source_key}/permissions/#{user}")
         end
 
-        def update_user_permissions
-
+        def update_user_permissions(content_source_key, user, permissions)
+          post("ent/sources/#{content_source_key}/permissions/#{user}", :permissions => permissions)
         end
 
-        def add_user_permissions
-
+        def add_user_permissions(content_source_key, user, permissions)
+          post("ent/sources/#{content_source_key}/permissions/#{user}/add", :permissions => permissions)
         end
 
-        def remove_user_permissions
-
+        def remove_user_permissions(content_source_key, user, permissions)
+          post("ent/sources/#{content_source_key}/permissions/#{user}/remove", :permissions => permissions)
         end
 
       end
